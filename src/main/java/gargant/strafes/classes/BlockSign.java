ba5d00087bd779ca7436c341ea692ddec7ca75b1
Gargant <gargant0373@gmail.com>
2023-05-08 13:36:32 +0200

    Implemented OG signs.

--- src/main/java/gargant/strafes/classes/BlockSign.java
+++ src/main/java/gargant/strafes/classes/BlockSign.java
@@ -1,5 +1,8 @@
 package gargant.strafes.classes;
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import masecla.mlib.main.MLib;
@@ -11,9 +14,11 @@ public class BlockSign {
     private int level = 0;
     private int duration = 20;
 
-    public BlockSign(MLib lib, String[] lines) {
+    public BlockSign(MLib lib, Block b, String[] lines) {
+        if(ogPowerup(b, lines)) return;
+
         this.powerup = lines[0];
-        
+
         try {
             this.level = Integer.parseInt(lines[1]);
         } catch (NumberFormatException ex) {
@@ -25,5 +30,18 @@ public class BlockSign {
         } catch (NumberFormatException ex) {
             lib.getLoggerAPI().error("Invalid number formatted in powerup sign! [" + lines[2] + "]");
         }
+    }
+
+    private boolean ogPowerup(Block b, String[] lines) {
+        try {
+            this.level = Integer.parseInt(lines[0]);
+            this.duration = Integer.parseInt(lines[1]) * 20;
+            if (b.getType().equals(Material.REDSTONE_BLOCK)) this.powerup = "speed";
+            else if (b.getType().equals(Material.EMERALD_BLOCK)) this.powerup = "jump";
+            else return false;
+        } catch (NumberFormatException ex) {
+            return false;
+        }
+        return true;
     }
 }
